259a551ed2ee0fe5f474d7a95d4dcea6b6e333cd
Ivan Nikolić <niksy5@gmail.com>
2014-08-05 10:07:04 +0200

    Add ARIA `role=link` for unfocusable elements
    As per http://www.heydonworks.com/article/reinventing-the-hyperlink
    
    Le sigh -_-

--- src/kist-fauxAnchor.js
+++ src/kist-fauxAnchor.js
@@ -24,14 +24,17 @@
 
 			// Make unfocusable elements focusable
 			if ( this.options.type === 'unfocusable' && this.options.focus ) {
-				this.dom.el.attr('tabindex', 0);
+				this.dom.el.attr({
+					'tabindex': 0,
+					'role': 'link'
+				});
 			}
 		},
 		destroy: function () {
 			this.dom.el.removeClass(plugin.classes.item);
 
 			if ( this.options.type === 'unfocusable' && this.options.focus ) {
-				this.dom.el.removeAttr('tabindex');
+				this.dom.el.removeAttr('tabindex role');
 			}
 		}
 	};
