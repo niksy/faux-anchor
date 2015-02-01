e12f39c81f1d214aca8d1cdf3839d584cfcd3b59
Ivan Nikolić <niksy5@gmail.com>
2015-02-01 17:03:20 +0100

    Release 0.5.1

--- dist/kist-fauxanchor.js
+++ dist/kist-fauxanchor.js
@@ -1,4 +1,4 @@
-/*! kist-fauxanchor 0.5.0 - Simulate default anchor action. | Author: Ivan Nikolić <niksy5@gmail.com> (http://ivannikolic.com/), 2015 | License: MIT */
+/*! kist-fauxanchor 0.5.1 - Simulate default anchor action. | Author: Ivan Nikolić <niksy5@gmail.com> (http://ivannikolic.com/), 2015 | License: MIT */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self);var n=f;n=n.jQuery||(n.jQuery={}),n=n.fn||(n.fn={}),n.fauxAnchor=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
 var $ = (typeof window !== "undefined" ? window.$ : typeof global !== "undefined" ? global.$ : null);
@@ -682,16 +682,16 @@ var $ = (typeof window !== "undefined" ? window.$ : typeof global !== "undefined
 var meta = require(11);
 var instance = 0;
 
-var instance = module.exports = {
+var instanceObj = module.exports = {
 	count: 0,
 	setup: function () {
 		this.uid = instance++;
 		this.ens = meta.ns.event + '.' + this.uid;
-		instance.count++;
+		instanceObj.count++;
 	},
 	destroy: function () {
 		$.removeData(this.element, meta.name);
-		instance.count--;
+		instanceObj.count--;
 	}
 };
 
